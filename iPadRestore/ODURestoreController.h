572b92f5db410511b146a098aba698f04a9cf06c
Will Ross <paxswill@gmail.com>
2011-01-31 10:12:32 -0500

    Added the actual UI

---
+++ iPadRestore/ODURestoreController.h
@@ -0,0 +1,17 @@
+//
+//  ODURestoreController.h
+//  iPadRestore
+//
+//  Created by Will Ross on 1/31/11.
+//  Copyright 2011 Naval Research Lab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ODURestoreController : NSViewController {
+@private
+    
+}
+
+@end
---
+++ iPadRestore/ODURestoreController.m
@@ -0,0 +1,28 @@
+//
+//  ODURestoreController.m
+//  iPadRestore
+//
+//  Created by Will Ross on 1/31/11.
+//  Copyright 2011 Naval Research Lab. All rights reserved.
+//
+
+#import "ODURestoreController.h"
+
+
+@implementation ODURestoreController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    // Clean-up code here.
+    
+    [super dealloc];
+}
+
+@end
